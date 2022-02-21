f68daaf432eab360c44514b20b79e607ebd8a3c7
Eduard Cosmin <cosmyn91d@yahoo.com>
2022-02-21 10:21:12 +0200

    Added Discount Entity

---
+++ src/Core/Shop.Core.DomainEntities/Discount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.DomainEntities
+{
+    public class Discount
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+        public int DiscountTypeId { get; set; }
+    }
+}
