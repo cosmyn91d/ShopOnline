696c45d090f3e41454fbbc77d1002ffe089d99b6
Alexanda Sauchea <alexandrateodora.sauchea@endava.com>
2022-02-21 10:32:31 +0200

    add product entity

---
+++ src/Core/Shop.Core.DomainEntities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.DomainEntities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int CategoryId { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime ModifyTime { get; set; }
+        public bool IsDisabled { get; set; }
+        public bool IsDeleted { get; set; }
+        public List<int> Discounts { get; set; }
+        public List<Attachment> Attachments { get; set; }
+    }
+}
