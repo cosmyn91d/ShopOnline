26eb9ce7febc9921dee827ba9fbbc8a33535cd20
Eduard Cosmin <cosmyn91d@yahoo.com>
2022-02-22 18:58:29 +0200

    Refactored entities :Discount and DiscountType

---
+++ src/Core/Shop.Core.DomainEntities/Discount.cs
@@ -0,0 +1,8 @@
+﻿namespace Shop.Core.DomainEntities;
+    public class Discount
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+        public int DiscountTypeId { get; set; }
+    }
+
--- src/Core/Shop.Core.DomainEntities/DiscountType.cs
+++ src/Core/Shop.Core.DomainEntities/DiscountType.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shop.Core.DomainEntities
-{
+﻿namespace Shop.Core.DomainEntities;
     public class DiscountType
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = String.Empty;
     }
-}
+
