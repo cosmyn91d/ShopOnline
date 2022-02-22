01c06816f7ec9253f57e2e5fe8175bc720d9ecf4
Alexanda Sauchea <alexandrateodora.sauchea@endava.com>
2022-02-22 09:48:46 +0200

    clean up code

--- src/Core/Shop.Core.DomainEntities/Attachment.cs
+++ src/Core/Shop.Core.DomainEntities/Attachment.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace Shop.Core.DomainEntities;
 
-namespace Shop.Core.DomainEntities
-{
     public class Attachment
     {
         public int Id { get; set; }
-        public string Path { get; set; }
+        public string? Path { get; set; }
     }
-}
+
