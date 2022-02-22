82c81b2f04e20ec29ebd0cb6a11661633b143db6
Alexanda Sauchea <alexandrateodora.sauchea@endava.com>
2022-02-22 11:02:51 +0200

    clean the code

--- src/Core/Shop.Core.Business/Interfaces/IProductService.cs
+++ src/Core/Shop.Core.Business/Interfaces/IProductService.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Shop.Core.DomainEntities;
 
-namespace Shop.Core.Business.Interfaces
-{
+namespace Shop.Core.Business.Interfaces;
+
     public interface IProductService
     {
-        public ProductView GetProductById(int id);
+        public Product GetProductById(int id);
+
         public List<Product> GetAllProducts();
-        public void AddProduct(ProductModel productModel);
-        public void EditProduct(int id, ProductModel productModel);
+        public void AddProduct(Product productModel);
+        public void EditProduct(int id, Product productModel);
         public void DeleteProduct(int id);
 
-
     }
-}
+
