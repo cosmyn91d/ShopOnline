790fe7cadb21f69621ca945fd9ab7e0ed16b0cc0
Alexanda Sauchea <alexandrateodora.sauchea@endava.com>
2022-02-23 12:12:01 +0200

    add product repo

---
+++ src/Core/Shop.Core.RepositoriesInterface/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Shop.Core.DomainEntities;
+
+namespace Shop.Core.RepositoriesInterface.Interfaces;
+
+public interface IProductRepository
+{
+    public Product GetProductById(int id);
+    public IEnumerable<Product> GetAllProducts();
+    public bool AddProduct(Product productModel);
+    public bool EditProduct(Product productModel);
+    public bool DeleteProduct(Product product);
+    public void SaveSchanges();
+}
