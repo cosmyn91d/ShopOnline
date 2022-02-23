9bab0deca6b542787724498592713fdb48c62d84
Eduard Cosmin <cosmyn91d@yahoo.com>
2022-02-23 10:13:41 +0200

    Fix  Ident

--- src/Core/Shop.Core.DomainEntities/Discount.cs
+++ src/Core/Shop.Core.DomainEntities/Discount.cs
@@ -1,8 +1,8 @@
 ﻿namespace Shop.Core.DomainEntities;
-    public class Discount
-    {
-        public int Id { get; set; }
-        public decimal Value { get; set; }
-        public int DiscountTypeId { get; set; }
-    }
 
+public class Discount
+{
+    public int Id { get; set; }
+    public decimal Value { get; set; }
+    public int DiscountTypeId { get; set; }
+}
