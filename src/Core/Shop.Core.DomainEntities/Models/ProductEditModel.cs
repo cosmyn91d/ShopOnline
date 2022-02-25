8127a33ff4d2894dee6e48296da990aef6908f9f
Alexanda Sauchea <alexandrateodora.sauchea@endava.com>
2022-02-25 14:59:09 +0200

    add ProductAddModel.cs and ProductEditModel.cs

---
+++ src/Core/Shop.Core.DomainEntities/Models/ProductEditModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop.Core.DomainEntities.Models;
+
+public class ProductEditModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Code { get; set; } = string.Empty;
+    public int CategoryId { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public bool IsDisabled { get; set; } = false;
+    public bool IsDeleted { get; set; } = false;
+}
